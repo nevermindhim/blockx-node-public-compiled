6703dfca96d9af93d35473ea9bdd3d6b6d32abc8
breavedev <scott.brown0629@gmail.com>
2022-11-01 20:40:48 -0500

    Update run-fullnode.sh

--- run-fullnode.sh
+++ run-fullnode.sh
@@ -11,4 +11,4 @@ cp genesis.json $HOME/.blockxd/config
 cd $HOME/.blockxd/config
 sed -i 's/seed_mode = false/seed_mode = true/g' config.toml
 sed -i 's/persistent_peers = \"\"/persistent_peers = \"8339668165d1fe98f24c042e7a61fd7c6aa7a1aa@142.93.202.64:26656\"/g' config.toml
-blockxd start --minimum-gas-prices=1000000000abcx
+service blockxd start

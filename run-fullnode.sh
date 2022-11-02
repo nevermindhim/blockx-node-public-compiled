e3db6b58a653001c8ecf58287a4b141f66ecc3ae
breavedev <scott.brown0629@gmail.com>
2022-11-01 21:30:58 -0500

    Update run-fullnode.sh

--- run-fullnode.sh
+++ run-fullnode.sh
@@ -9,8 +9,13 @@ blockxd init seednode2 --chain-id blockx_12345-1 --keyring-backend file
 
 cp genesis.json $HOME/.blockxd/config
 cd $HOME/.blockxd/config
-sed -i 's/seed_mode = false/seed_mode = true/g' config.toml
-sed -i 's/persistent_peers = \"\"/persistent_peers = \"8339668165d1fe98f24c042e7a61fd7c6aa7a1aa@142.93.202.64:26656\"/g' config.toml
+
+# for seed nodes, run following.
+# sed -i 's/seed_mode = false/seed_mode = true/g' config.toml
+# sed -i 's/persistent_peers = \"\"/persistent_peers = \"8339668165d1fe98f24c042e7a61fd7c6aa7a1aa@142.93.202.64:26656\"/g' config.toml
+
+# for non-seed nodes, run following.
+sed -i 's/seeds = \"\"/seeds = \"8339668165d1fe98f24c042e7a61fd7c6aa7a1aa@142.93.202.64:26656,20ac427a3d3cbabf5818e87056438495ee5dc2c3@142.93.3.163:26656,b12d15da3ef70b306aec7a4a240f81b497033837@167.99.12.133:26656\"/g' config.toml
 
 cp blockxd.service /etc/systemd/system
 service blockxd start

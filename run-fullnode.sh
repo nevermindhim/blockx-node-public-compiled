6bf2dc9390d483b0ab30a602e1d71f82c389767f
breavedev <scott.brown0629@gmail.com>
2022-11-01 20:38:45 -0500

    Update run-fullnode.sh

--- run-fullnode.sh
+++ run-fullnode.sh
@@ -10,5 +10,5 @@ blockxd init seednode2 --chain-id blockx_12345-1 --keyring-backend file
 cp genesis.json $HOME/.blockxd/config
 cd $HOME/.blockxd/config
 sed -i 's/seed_mode = false/seed_mode = true/g' config.toml
-sed -i 's/persistent_peers = \"\"/persistent_peers = \"168f6c74b791a28a173eae5e999bae1d1357a2c3@142.93.202.64:26656\"/g' config.toml
+sed -i 's/persistent_peers = \"\"/persistent_peers = \"8339668165d1fe98f24c042e7a61fd7c6aa7a1aa@142.93.202.64:26656\"/g' config.toml
 blockxd start --minimum-gas-prices=1000000000abcx

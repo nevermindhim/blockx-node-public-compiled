3effd9d55bd1c1c4aca7ff2d684d4022592fc8be
breavedev <scott.brown0629@gmail.com>
2022-11-01 21:41:44 -0500

    Update run-fullnode.sh

--- run-fullnode.sh
+++ run-fullnode.sh
@@ -8,6 +8,7 @@ blockxd config keyring-backend file
 blockxd init seednode2 --chain-id blockx_12345-1 --keyring-backend file
 
 cp genesis.json $HOME/.blockxd/config
+cp blockxd.service /etc/systemd/system
 cd $HOME/.blockxd/config
 
 # for seed nodes, run following.
@@ -17,7 +18,6 @@ cd $HOME/.blockxd/config
 # for non-seed nodes, run following.
 sed -i 's/seeds = \"\"/seeds = \"8339668165d1fe98f24c042e7a61fd7c6aa7a1aa@142.93.202.64:26656,20ac427a3d3cbabf5818e87056438495ee5dc2c3@142.93.3.163:26656,b12d15da3ef70b306aec7a4a240f81b497033837@167.99.12.133:26656\"/g' config.toml
 
-cp ./blockxd.service /etc/systemd/system
 systemctl daemon-reload
 systemctl start blockxd.service
 service blockxd start

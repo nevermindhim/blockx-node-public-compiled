9fae0e893da05267959a8340cde4b0165439d948
breavedev <scott.brown0629@gmail.com>
2022-11-02 00:13:02 -0500

    Update run-fullnode.sh

--- run-fullnode.sh
+++ run-fullnode.sh
@@ -17,10 +17,10 @@ cd $HOME/.blockxd/config
 
 # for seed nodes, run following. need to change persistent_peers
 # sed -i 's/seed_mode = false/seed_mode = true/g' config.toml
-sed -i 's/persistent_peers = \"\"/persistent_peers = \"8339668165d1fe98f24c042e7a61fd7c6aa7a1aa@142.93.202.64:26656,b12d15da3ef70b306aec7a4a240f81b497033837@167.99.12.133:26656\"/g' config.toml
+sed -i 's/persistent_peers = \"\"/persistent_peers = \"8339668165d1fe98f24c042e7a61fd7c6aa7a1aa@142.93.202.64:26656,23b12388a6922cb3bb7b72b8b7cc7429b7444f80@142.93.3.163:26656,85f8e76274cb95d9dde3487d5b8547be27f58192@167.99.12.133:26656\"/g' config.toml
 
 # for non-seed nodes, run following.
-# sed -i 's/seeds = \"\"/seeds = \"8339668165d1fe98f24c042e7a61fd7c6aa7a1aa@142.93.202.64:26656,20ac427a3d3cbabf5818e87056438495ee5dc2c3@142.93.3.163:26656,b12d15da3ef70b306aec7a4a240f81b497033837@167.99.12.133:26656\"/g' config.toml
+# sed -i 's/seeds = \"\"/seeds = \"8339668165d1fe98f24c042e7a61fd7c6aa7a1aa@142.93.202.64:26656,23b12388a6922cb3bb7b72b8b7cc7429b7444f80@142.93.3.163:26656,85f8e76274cb95d9dde3487d5b8547be27f58192@167.99.12.133:26656\"/g' config.toml
 
 systemctl daemon-reload
 systemctl start blockxd.service

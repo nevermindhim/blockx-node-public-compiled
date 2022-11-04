193b744a5d4f6db27cea2e49f7a3a3bbabea35a7
breavedev <scott.brown0629@gmail.com>
2022-11-04 03:17:53 -0500

    Update run-fullnode.sh

--- run-fullnode.sh
+++ run-fullnode.sh
@@ -12,6 +12,7 @@ blockxd init seednode2 --chain-id blockx_12345-1 --keyring-backend file
 
 cp genesis.json $HOME/.blockxd/config
 
+sed -i 's/ExecStart=blockxd start/ExecStart=$(which blockxd) start/g' config.toml
 sudo cp blockxd.service /etc/systemd/system
 cd $HOME/.blockxd/config
 

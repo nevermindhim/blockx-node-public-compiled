0330bdfff49ec89461684355940e510934c50744
breavedev <scott.brown0629@gmail.com>
2022-11-04 03:04:04 -0500

    added sudo

--- run-fullnode.sh
+++ run-fullnode.sh
@@ -1,8 +1,8 @@
-systemctl stop blockxd.service
+sudo systemctl stop blockxd.service
 
 rm -rf $HOME/.blockxd
 rm $HOME/go/bin/blockxd
-rm /etc/systemd/system/blockxd.service
+sudo rm /etc/systemd/system/blockxd.service
 
 cp blockxd $HOME/go/bin
 
@@ -12,7 +12,7 @@ blockxd init seednode2 --chain-id blockx_12345-1 --keyring-backend file
 
 cp genesis.json $HOME/.blockxd/config
 
-cp blockxd.service /etc/systemd/system
+sudo cp blockxd.service /etc/systemd/system
 cd $HOME/.blockxd/config
 
 # for seed nodes, run following. need to change persistent_peers
@@ -22,5 +22,5 @@ cd $HOME/.blockxd/config
 # for non-seed nodes, run following.
 sed -i 's/seeds = \"\"/seeds = \"8339668165d1fe98f24c042e7a61fd7c6aa7a1aa@142.93.202.64:26656,23b12388a6922cb3bb7b72b8b7cc7429b7444f80@142.93.3.163:26656,85f8e76274cb95d9dde3487d5b8547be27f58192@167.99.12.133:26656\"/g' config.toml
 
-systemctl daemon-reload
-systemctl start blockxd.service
+sudo systemctl daemon-reload
+sudo systemctl start blockxd.service

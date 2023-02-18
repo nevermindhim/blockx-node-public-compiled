936afd405dde357cd2564ceefb3f24919b76bd88
breavedev <scott.brown0629@gmail.com>
2023-02-18 15:55:16 +0200

    fix: create-validator.sh

--- create-validator.sh
+++ create-validator.sh
@@ -10,16 +10,4 @@ COMMISSION_MAX_RATE=0.10            # Commission max rate
 COMMISSION_MAX_CHANGE_RATE=0.01     # Commission max change rate
 MIN_SELF_DELEGATION=1000bcx         # Min self delegation
 
-blockxd tx staking create-validator \
---amount=$AMOUNT \
---pubkey=$(blockxd tendermint show-validator) \
---moniker=$MONIKER \
---chain-id=$CHAINID \
---commission-rate=$COMMISSION_RATE \
---commission-max-rate=$COMMISSION_MAX_RATE \
---commission-max-change-rate=$COMMISSION_MAX_CHANGE_RATE \
---min-self-delegation=$MIN_SELF_DELEGATION \
---gas="300000" \
---gas-prices="1000000000abcx" \
---from=$KEY \
---keyring-backend $KEYRING
+blockxd tx staking create-validator --amount=$AMOUNT --pubkey=$(blockxd tendermint show-validator) --moniker=$MONIKER --chain-id=$CHAINID --commission-rate=$COMMISSION_RATE --commission-max-rate=$COMMISSION_MAX_RATE --commission-max-change-rate=$COMMISSION_MAX_CHANGE_RATE --min-self-delegation=$MIN_SELF_DELEGATION --gas="300000" --gas-prices="1000000000abcx" --from=$KEY --keyring-backend $KEYRING
